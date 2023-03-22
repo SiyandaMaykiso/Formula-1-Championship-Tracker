9784ba9005600e394ca5b7734a766c5394eadd1c
Siyanda Mayekiso <s.mayekiso@gmail.com>
2023-03-22 17:54:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,24 @@ app.controller("MainController", function($scope, $http) {
     $scope.drivers = response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
   });
 });
+
+const apiUrl = "https://ergast.com/api/f1/current/constructorStandings.json";
+
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    const standings = data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
+    
+    const table = document.querySelector("#constructor-standings tbody");
+    for (let i = 0; i < standings.length; i++) {
+      const row = table.insertRow(i);
+      const position = row.insertCell(0);
+      const constructor = row.insertCell(1);
+      const points = row.insertCell(2);
+      
+      position.textContent = standings[i].position;
+      constructor.textContent = standings[i].Constructor.name;
+      points.textContent = standings[i].points;
+    }
+  })
+  .catch(error => console.error(error));
